a29149255beb378964009bfece2d1e50c2ea04ba
堀川太一 <horikawataichi@horikawataichinoMacBook-Pro.local>
2020-03-06 17:15:19 +0900

    TodoControllerのリダイレクトをルートネームを使用した処理に変更

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -47,7 +47,7 @@ class TodoController extends Controller
         $input = $request->all();
         $input['user_id'] = Auth::id();
         $this->todo->fill($input)->save();
-        return redirect()->to('todo');
+        return redirect()->route('todo.index');
     }
 
     /**
@@ -84,7 +84,7 @@ class TodoController extends Controller
     {
         $input = $request->all();
         $this->todo->find($id)->fill($input)->save();
-        return redirect()->to('todo');
+        return redirect()->route('todo.index');
     }
 
     /**
@@ -96,6 +96,6 @@ class TodoController extends Controller
     public function destroy($id)
     {
         $this->todo->find($id)->delete();
-        return redirect()->to('todo');
+        return redirect()->route('todo.index');
     }
 }
